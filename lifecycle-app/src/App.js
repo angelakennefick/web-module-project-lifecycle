85229da8aaa7401d3b257815bc664f66b92001a6
Angela-Kennefick <angelack2000@gmail.com>
2021-05-04 22:49:54 -0500

    fetch

--- lifecycle-app/src/App.js
+++ lifecycle-app/src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+useEffect(() => {
+  fetch(https://api.github.com/users/angelakennefick)
+  .then(resp => {
+    if(resp.ok) {
+      return resp
+
+      
+    }
+  })
+})
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
